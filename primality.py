3c846f6049be051cde64b5d149826b350b014e69
vipul43 <saifunny43@gmail.com>
2020-04-24 11:07:53 +0530

    corrected the indentations of functions

--- primality.py
+++ primality.py
@@ -8,58 +8,58 @@
 """
 
 #SOLUTION-1: (BRUTE_FORCE) --> O(n)
-    def isPrime1(n: int) -> bool:
-        if(n<=1):
-            return False
-        elif(n==2):
-            return True
-        else:
-            for i in range(2, n):
-                if(n%i==0):
-                    return False
-            return True
+def isPrime1(n: int) -> bool:
+    if(n<=1):
+        return False
+    elif(n==2):
+        return True
+    else:
+        for i in range(2, n):
+            if(n%i==0):
+                return False
+        return True
             
 #SOLUTION-2: (BETTER_BRUTE_FORCE) --> O(n/2)==O(n)!
-    def isPrime2(n: int) -> bool:
-        if(n<=1):
-            return False
-        elif(n==2):
-            return True
-        else:
-            for i in range(2, n//2):
-                if(n%i==0):
-                    return False
-            return True
+def isPrime2(n: int) -> bool:
+    if(n<=1):
+        return False
+    elif(n==2):
+        return True
+    else:
+        for i in range(2, n//2):
+            if(n%i==0):
+                return False
+        return True
             
 #SOLUTION-3: (ALGORIHTHM) --> O(sqrt(n))
-    def isPrime3(n: int) -> bool:
-        if(n<=1):
-            return False
-        elif(n<=3):
-            return True
-        elif(n%2==0 or n%3==0):
-            return False
-        else:
-            for i in range(5, int(n**0.5)+1):
-                if(n%i==0):
-                    return False
-            return True
+def isPrime3(n: int) -> bool:
+    if(n<=1):
+        return False
+    elif(n<=3):
+        return True
+    elif(n%2==0 or n%3==0):
+        return False
+    else:
+        for i in range(5, int(n**0.5)+1):
+            if(n%i==0):
+                return False
+        return True
             
 #SOLUTION-4 (BETTER_ALGORITHM) --> O(sqrt(n)/3)==O(sqrt(n))!
-    def isPrime4(n: int) -> bool:
-        if(n<=1):
-            return False
-        elif(n<=3):
-            return True
-        elif(n%2==0 or n%3==0):
-            return False
-        else:
-            i=5
-            while(i<=int(a**0.5)):
-                if(n%i==0 or n%(i+2)==0):
-                    return False
-                i=i+6
-            return True
+def isPrime4(n: int) -> bool:
+    if(n<=1):
+        return False
+    elif(n<=3):
+        return True
+    elif(n%2==0 or n%3==0):
+        return False
+    else:
+        i=5
+        while(i<=int(a**0.5)):
+            if(n%i==0 or n%(i+2)==0):
+                return False
+            i=i+6
+        return True
  
  
 #DESCRIPTION:
